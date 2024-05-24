e0d74349c60e0278828ead537cfefcbf4cd9f747
auizadi <auizaldi.21026@mhs.unesa.ac.id>
2024-05-24 10:43:38 +0700

    commit second

---
+++ resources/views/kontak-page.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kontak | Masjid Mujahidin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styleCSS/kontak.css">
+</head>
+  <body>
+    
+
+  <div class="container rounded-4 shadow-lg mt-5">
+    <div class="row">
+      <div class="col-8">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident architecto molestiae, beatae praesentium nam quisquam atque aut natus nobis quasi, quaerat, minus in totam expedita dolore animi repellendus facilis.</p>
+      </div>
+      <div class="col-4 bg-success">
+        <h3 class="text-light fw-bold my-4">Informasi</h3>
+        <h2 class="text-light my-4">Kontak atau <br>Kunjungi Kami di:</h2>
+        <h3 class="text-light fw-bold my-4">ALAMAT SUREL</h3>
+        <h2 class="text-light my-4">admin@masjidmujahidin-sby.sch.id</h2>
+      </div>
+    </div>
+  </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
