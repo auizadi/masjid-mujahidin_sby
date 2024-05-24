87ce6ec2839615356a4cf039b7feb27e44e16727
auizadi <auizaldi.21026@mhs.unesa.ac.id>
2024-05-24 07:26:33 +0700

    commit awal

---
+++ resources/views/profil-page.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styleCSS/profil-style.css">
+  </head>
+  <body>
+   
+  <div class="container-fluid mt-4">
+     <div class="row justify-content-center">
+    <div class="col-6">
+      <h1>Profil Yayasan Mujahidin Surabaya</h1>
+      <h4>Membangun Harmoni, Menebar Kasih, Menyongsong Kemajuan Bersama Umat.</h4>
+    </div>
+    <div class="col-6">
+     <!-- <img src="../img/Logo Muhammadiyah.png" alt=""> -->
+    </div>
+  </div>
+  </div>
+
+  <!-- deskripsi -->
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col">
+        <p class="my-3 fw-bold text-success-emphasis">SEJARAH SINGKAT</p>
+        <h2 class="my-3">Yayasan Mujahidin Surabaya</h2>
+        <p class="text my-3">
+        Yayasan Masjid Mujahidin Surabaya memiliki sejarah yang kaya dan berakar dalam semangat kebersamaan dan keteguhan. Berdiri pada tahun [tahun berdiri], yayasan ini lahir dari tekad masyarakat Surabaya untuk memiliki pusat ibadah dan pusat kegiatan Islami yang berkualitas.
+        </p>
+        <p class="text my-3">
+        Seiring berjalannya waktu, yayasan ini berkembang pesat sebagai landasan bagi Masjid Mujahidin Surabaya, yang menjadi pusat kegiatan keagamaan dan sosial bagi umat Muslim di sekitarnya. Sejak awal, yayasan ini telah mengemban visi untuk memberdayakan masyarakat melalui pendidikan, kesejahteraan sosial, dan pengembangan spiritual.
+        </p>
+        <p class="text my-3">
+        Yayasan Masjid Mujahidin Surabaya telah menjadi tulang punggung berbagai kegiatan, termasuk pendidikan, pelatihan, serta bimbingan spiritual bagi masyarakat sekitar. Program-program sosial yang dijalankan oleh yayasan mencakup bantuan kemanusiaan, pengembangan ekonomi, dan pemberdayaan perempuan.
+        </p>
+        <p class="text my-3">
+        Selama perjalanan panjangnya, yayasan ini telah melibatkan banyak sukarelawan, dermawan, dan pemimpin komunitas yang berkomitmen untuk membangun keberlanjutan dan kesejahteraan bersama. Yayasan Masjid Mujahidin Surabaya juga menjadi tempat berbagai kegiatan keagamaan seperti kajian, tausiyah, dan kegiatan Islami lainnya.
+        </p>
+        <p class="text my-3">
+        Keberhasilan yayasan ini tidak terlepas dari dukungan dan partisipasi aktif masyarakat Surabaya yang turut serta membangun dan mendukung visi yayasan. Melalui perjalanan sejarahnya, Yayasan Masjid Mujahidin Surabaya terus menjadi pusat keberagaman, keberlanjutan, dan kebaikan bagi masyarakat setempat.
+        </p>
+        
+      </div>
+      <div class="col">
+        <img src="../img/Logo Muhammadiyah.png" alt="">
+      </div>
+    </div>
+  </div>
+ 
+  <!-- akhir deskripsi -->
+
+
+  <!-- penjelasan -->
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col text-center">
+        <img src="../img/learning.png" alt="">
+        <h3 class="fw-bold text-center">Pendidikan Berkualitas</h3>
+        <p class="text-deskripsi">Yayasan ini menawarkan program pendidikan yang berkualitas, baik di tingkat dasar, menengah, maupun tinggi. Program pendidikan yang terintegrasi dengan nilai-nilai keislaman & pengembangan berkarakter.</p>
+      </div>
+     
+      <div class="col text-center">
+      <img src="../img/learning.png" alt="">
+        <h3 class="fw-bold text-center">Pendidikan Berkualitas</h3>
+        <p class="text-deskripsi">Yayasan ini menawarkan program pendidikan yang berkualitas, baik di tingkat dasar, menengah, maupun tinggi. Program pendidikan yang terintegrasi dengan nilai-nilai keislaman & pengembangan berkarakter.</p>
+      </div>
+
+      <div class="col text-center">
+      <img src="../img/learning.png" alt="">
+        <h3 class="fw-bold text-center">Pendidikan Berkualitas</h3>
+        <p class="text-deskripsi">Yayasan ini menawarkan program pendidikan yang berkualitas, baik di tingkat dasar, menengah, maupun tinggi. Program pendidikan yang terintegrasi dengan nilai-nilai keislaman & pengembangan berkarakter.</p>
+      </div>
+
+
+    </div>
+  </div>
+
+  <!-- akhir penjelasan -->
+
+  <!-- sekilas -->
+
+  <div class="container rounded-3 bg-light my-4">
+    <div class="container p-2">
+      <div class="row">
+      <div class="col">
+        <img src="../img/Logo Muhammadiyah.png" alt="">
+      </div>
+      <div class="col">
+        <h4 class="my-3 fw-bold">SEKILAS TENTANG</h4>
+        <p class="display-3">Cerita Kami</p>
+        <p class="text">Yayasan Masjid Mujahidin Surabaya adalah cerminan kepedulian dan dedikasi kami untuk membentuk masyarakat yang berkualitas, berakhlak mulia, dan berdaya saing. Berawal dari tekad kuat dan semangat berbagi, kami memulai perjalanan kami di kota Surabaya.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p class="text-center my-3 display-3 text-success fw-semibold">400+</p>
+        <p class="text-center my-3 text-secondary">SISWA SISWI</p>
+      </div>
+      <div class="col">
+        <p class="text-center my-3 display-3 text-success fw-semibold">400+</p>
+        <p class="text-center my-3 text-secondary">SISWA SISWI</p>
+      </div>
+      <div class="col">
+        <p class="text-center my-3 display-3 text-success fw-semibold">400+</p>
+        <p class="text-center my-3 text-secondary">SISWA SISWI</p>
+      </div>
+      <div class="col">
+        <p class="text-center my-3 display-3 text-success fw-semibold">400+</p>
+        <p class="text-center my-3 text-secondary">SISWA SISWI</p>
+      </div>
+      
+    </div>
+    </div>
+    
+  </div>
+
+  <!-- akhir sekilas -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
